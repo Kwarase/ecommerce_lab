66def69b09103e6333a7c58956647e04b19f032e
Emmanuel Kwarase <112888639+Kwarase@users.noreply.github.com>
2022-10-21 11:54:38 +0000

    Update index.php

--- register/index.php
+++ register/index.php
@@ -6,14 +6,13 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Menu</title>
-   
 </head>
 
 <body>
 
-<p><a href="../register/Login/login.php" class="btn btn-primary btn-lg" role="button">Login</a></p>
+<p><a href="../Login/login.php" class="btn btn-primary btn-lg" role="button">Login</a></p>
 
-<p><a href="../register/Login/register.php" class="btn btn-primary btn-lg" role="button">Register</a></p>
+<p><a href="../Login/register.php" class="btn btn-primary btn-lg" role="button">Register</a></p>
 
 
 </body>
