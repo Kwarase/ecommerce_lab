41604a2f9288b249e63f3a5f4fe1d3ec54e100dc
Emmanuel Kwarase <112888639+Kwarase@users.noreply.github.com>
2022-10-31 14:35:37 +0000

    Add files via upload

---
+++ login/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Menu</title>
+   
+</head>
+
+<body>
+
+<p><a href="../register/login/login.php" class="btn btn-primary btn-lg" role="button">Login</a></p>
+
+<p><a href="../register/login/register.php" class="btn btn-primary btn-lg" role="button">Register</a></p>
+
+
+</body>
+
+</html>
+
+
+	
---
+++ login/login/registerprocess.php
@@ -0,0 +1,31 @@
+<?php
+require("../controllers/customer_controller.php");
+
+$name = $_POST['fullname'];
+$email = $_POST['email'];
+$password = $_POST['password'];
+$country = $_POST['country'];
+$city = $_POST['city'];
+$contact = $_POST['contact'];
+$image = $_POST['image'];
+
+
+// encrypting the the password using hash.
+$hash = password_hash($password, PASSWORD_DEFAULT);
+
+// check whether function works
+$check= add_customer_ctrl($name, $email, $hash, $country, $city, $contact, $image);
+
+if ($check) {
+	echo "Registration Successful";
+	header("Location: ../Login/login.php");
+}
+
+else{
+	echo "not working";
+}
+
+
+
+
+?>
