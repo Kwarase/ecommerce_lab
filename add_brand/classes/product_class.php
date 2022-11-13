a4b67578cd8a51034e45d3c3adbb389c24ed5e62
Emmanuel Kwarase <112888639+Kwarase@users.noreply.github.com>
2022-11-13 00:52:24 +0000

    Add files via upload

---
+++ add_brand/classes/product_class.php
@@ -0,0 +1,40 @@
+<?php
+//connect to database class
+require("../settings/db_class.php");
+
+/**
+*Product class to handle all product functions 
+*/
+/**
+ *@author Emmanuel Kwarase
+ *
+ */
+
+class general_class extends db_connection
+{
+	//--INSERT--//
+
+	function insert_brand($a){
+
+		$sql = "INSERT INTO `brands`(`brand_name`) VALUES ('$a')";
+
+		return $this -> db_query($sql);
+	}
+
+	//--SELECT--//
+	//Select all
+	function select_brand(){
+		$sql =" SELECT * FROM `brands`";
+
+		return $this -> db_fetch_all($sql);
+	}
+
+	//Select one
+	function select_one_brand($bid){
+		$sql =" SELECT * FROM `brands` WHERE `brand_id` = '$bid'";
+
+		return $this -> db_fetch_one($sql);
+	}
+
+}
+?>
