02f434253ad7db93bab6d55df4bfe2a64d7b4887
Emmanuel Kwarase <112888639+Kwarase@users.noreply.github.com>
2022-11-13 01:03:06 +0000

    Delete login directory

--- login/login/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-
-//function to check for logout
-    unset ($_SESSION["customer_id"]);
-    unset ($_SESSION["user_role"]);
-    header('Location: ../index.php');
-
-?>
--- login/settings/db_cred.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-//Database credentials
-define("DATABASE", "heroku_3913570f2761d87");
-define("SERVER", "eu-cdbr-west-03.cleardb.net");
-define("USERNAME", "b26bb63832708b");
-define("PASSWD", "1a6fad73");
-
-?>
