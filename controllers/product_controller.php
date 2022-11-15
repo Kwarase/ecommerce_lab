a9d5dd547ec427bf31c01cf9b413b37f81982abc
Emmanuel Kwarase <112888639+Kwarase@users.noreply.github.com>
2022-11-15 11:21:07 +0000

    Update product_controller.php

--- controllers/product_controller.php
+++ controllers/product_controller.php
@@ -2,14 +2,12 @@
 //connect to the user account class
 include("../classes/product_class.php");
 
-// //sanitize data
-// function cleanText($data) 
-// {
-//   $data = trim($data);
-//   //$data = stripslashes($data);
-//   //$data = htmlspecialchars($data);
-//   return $data;
-// }
+//sanitize data
+function cleanText($data) 
+  {
+    $data = trim($data);
+    return $data;
+  }
 
 
 
@@ -58,6 +56,53 @@ function update_all_brands_ctrl($bid, $bname){
   // return method
   $data = $update_brand -> update_brand_cls($bid, $bname);
     return $data;
+}
+
+
+/* CATEGORIES */
+//insert
+function add_category_ctrl($a){
+
+  //creating an instance
+  $add_category = new general_class();
+
+  // return method
+  return $add_category -> insert_category($a);
+}
+
+//--SELECT--//
+//selecting one categories
+function select_category_ctrl($cat_id){
+
+  // creating instance
+  $select_category = new general_class();
+
+  // return method
+  $data = $select_category -> select_one_category($cat_id);
+    return $data;
+}
+
+//selecting all categories
+function select_all_category_ctrl(){
+
+  // creating instance
+  $select_category = new general_class();
+
+  // return method
+  $data = $select_category -> select_category();
+    return $data;
+}
+
+//--UPDATE--//
+//update all categories
+function update_all_category_ctrl($cat_id, $cat_name){
+
+  //creating instance
+  $update_category = new general_class();
+
+  // return method
+  return $update_category -> update_category_cls($cat_id, $cat_name);
+  
 }
 
 
