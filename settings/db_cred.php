1d21907afa8640ef144ddd1b459b4b42dd11cc26
Emmanuel Kwarase <112888639+Kwarase@users.noreply.github.com>
2022-11-15 10:37:06 +0000

    Update db_cred.php

--- settings/db_cred.php
+++ settings/db_cred.php
@@ -3,6 +3,6 @@
 define("DATABASE", "heroku_3913570f2761d87");
 define("SERVER", "eu-cdbr-west-03.cleardb.net");
 define("USERNAME", "b26bb63832708b");
-define("PASSWD", "");
+define("PASSWD", "1a6fad73");
 
 ?>
